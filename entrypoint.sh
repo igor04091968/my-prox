a7e9d80654fc6eb071d80ed4d9564dede2005439
IgorRachkov <89467086+igor04091968@users.noreply.github.com>
2025-09-22 13:00:04 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@
 (
   while true; do
     echo "--- Starting Chisel client ---"
-    chisel client vds1.iri1968.dpdns.org:4443 R:8181:localhost:443
+    chisel client vds1.iri1968.dpdns.org:4443 R:8282:localhost:443
     echo "--- Chisel client disconnected. Reconnecting in 5 seconds. ---"
     sleep 5
   done
